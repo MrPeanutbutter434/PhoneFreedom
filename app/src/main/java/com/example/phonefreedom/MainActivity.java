2699c3228ac8ab8031bfbd00f8e7e657b20bbad0
Peanutbutter <prionslop@gmail.com>
2021-02-16 11:12:40 +0530

    Add boilerplate for app picker

--- app/src/main/java/com/example/phonefreedom/MainActivity.java
+++ app/src/main/java/com/example/phonefreedom/MainActivity.java
@@ -5,6 +5,8 @@ package com.example.phonefreedom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.net.Uri;
@@ -15,11 +17,14 @@ import android.provider.Settings;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -31,6 +36,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private int timeOut;
     private WindowManager windowManager;
     private ImageView imageView;
+    private Context mContext;
+    private RelativeLayout mRelativeLayout;
+    private Button mButton;
+    private PopupWindow mPopupWindow;
+    private Activity mActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         
         initSpinner();
         initButton();
+        initAppPicker();
 
         if (!Settings.canDrawOverlays(this)) {
             askPermission();
@@ -143,5 +155,83 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         };
 
         timer.start();
+    }
+
+    private void initAppPicker() {
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Get the activity
+        mActivity = MainActivity.this;
+
+        // Get the widgets reference from XML layout
+        mRelativeLayout = findViewById(R.id.app_picker_layout);
+        mButton = findViewById(R.id.app_picker);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initialize a new instance of LayoutInflater service
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                // Inflate the custom layout/view
+                View customView = inflater.inflate(R.layout.app_picker, (ViewGroup) findViewById(R.id.main_activity), false);
+
+                /*
+                    public PopupWindow (View contentView, int width, int height)
+                        Create a new non focusable popup window which can display the contentView.
+                        The dimension of the window must be passed to this constructor.
+
+                        The popup does not provide any background. This should be handled by
+                        the content view.
+
+                    Parameters
+                        contentView : the popup's content
+                        width : the popup's width
+                        height : the popup's height
+                */
+                // Initialize a new instance of popup window
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+
+                // Get a reference for the custom view close button
+                Button closeButton = customView.findViewById(R.id.app_picker_close);
+
+                // Set a click listener for the popup window close button
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Dismiss the popup window
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                /*
+                    public void showAtLocation (View parent, int gravity, int x, int y)
+                        Display the content view in a popup window at the specified location. If the
+                        popup window cannot fit on screen, it will be clipped.
+                        Learn WindowManager.LayoutParams for more information on how gravity and the x
+                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
+                        to specifying Gravity.LEFT | Gravity.TOP.
+
+                    Parameters
+                        parent : a parent view to get the getWindowToken() token from
+                        gravity : the gravity which controls the placement of the popup window
+                        x : the popup's x location offset
+                        y : the popup's y location offset
+                */
+                // Finally, show the popup window at the center location of root relative layout
+                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
+            }
+        });
     }
 }
