3670a60c44f93c5a28db4b76418c429e91bd79fc
Peanutbutter <prionslop@gmail.com>
2021-02-17 19:50:41 +0530

    Added data model for app picker

---
+++ app/src/main/java/com/example/phonefreedom/AppPickerDataModel.java
@@ -0,0 +1,11 @@
+package com.example.phonefreedom;
+
+public class AppPickerDataModel {
+    private String appName;
+    private boolean checked;
+
+    AppPickerDataModel(String appName, boolean checked) {
+        this.appName = appName;
+        this.checked = checked;
+    }
+}
